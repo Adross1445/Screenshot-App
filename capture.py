90097f7cf310c60552191756b2b1668410cd4eb0
Adross1445 <adross1445@gmail.com>
2025-01-10 15:06:18 -0600

    Documentation

--- capture.py
+++ capture.py
@@ -3,25 +3,33 @@ import time
 import pyscreenshot as ImageGrab 
 import schedule 
 
-
 from datetime import datetime 
 
 
 def take_screen_shot():
-    print("Taking screenshot...")
-    image_name = f"screenshot-{str(datetime.now())}"
-    screenshot = ImageGrab.grab()
+    """
+    Takes a screenshot of the device's screens and sends it to the
+    Screenshots folder located in the app folder. Also has text outout
+    informing the user of the screenshot's status.
+    """
+    print("Taking screenshot...") 
+    image_name = f"screenshot-{str(datetime.now())}" # Names the image "screenshot-date&time of capture".
+    screenshot = ImageGrab.grab() # Module capturing the screenshot.
 
-    filepath = f"./Screenshots/{image_name}.png"
+    filepath = f"./Screenshots/{image_name}.png" # Creates a variable of the screenshot's storage destination.
  
-    screenshot.save(filepath)
+    screenshot.save(filepath) 
 
     print("Screenshot taken.")
 
     return filepath
 
 def main():
-    schedule.every(1).hour.do(take_screen_shot)
+    """
+    Schedules a screenshot to be taken every hour and to pause for 1
+    second to allow it to start again.
+    """
+    schedule.every(1).hour.do(take_screen_shot) # Takes the screenshot every hour.
 
     while True:
         schedule.run_pending()
